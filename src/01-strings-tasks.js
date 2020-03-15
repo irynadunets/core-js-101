7de83047ed4701c14411c5110b74406de285e28f
Iryna Dunets <idunets@yahoo.com>
2020-03-15 13:33:08 +0200

    Update 01-strings-tasks.js

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -20,13 +20,12 @@
  */
 function concatenateStrings( value1, value2 ) {
   if(value1 === ''){
-return value2;
-}else if(value2 === ''){
-return value1;
-}else {
-return value1.concat(value2);
+     return value2;
+  }else if(value2 === ''){
+     return value1;
+  }else {
+     return value1.concat(value2);
 }
-
 }
 
 
