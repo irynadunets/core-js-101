d4cfaa4bf5c3740aa3a5556b6ecdb38d7e879c9e
Iryna Dunets <idunets@yahoo.com>
2020-03-15 23:28:46 +0200

    Update 01-strings-tasks.js

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -269,7 +269,7 @@ function encodeToRot13( str ) {
 function isString(value) {
   if (typeof value === 'string' || value instanceof String){
     return true;
-  }else{
+  }else {
      return false;
   }
 }
