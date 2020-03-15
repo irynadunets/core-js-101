cd72106496ddf939b27219a2707d8236ca2197df
Iryna Dunets <idunets@yahoo.com>
2020-03-15 13:31:03 +0200

    Update 01-strings-tasks.js

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -18,8 +18,15 @@
  *   'aa',''    => 'aa'
  *   '',  'bb'  => 'bb'
  */
-function concatenateStrings(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function concatenateStrings( value1, value2 ) {
+  if(value1 === ''){
+return value2;
+}else if(value2 === ''){
+return value1;
+}else {
+return value1.concat(value2);
+}
+
 }
 
 
